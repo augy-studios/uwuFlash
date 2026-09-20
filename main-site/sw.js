a01b3b786fd0f8a3cd45fe0ab9ee38060981b882
Augy <ateoks@gmail.com>
2026-09-20 18:46:53 +0800

    initial structure

--- main-site/sw.js
+++ main-site/sw.js
@@ -1,12 +1,12 @@
-const CACHE = "template-offline-v1";
+const CACHE = "uwuFlash-v1";
 
 const ASSETS = [
   "/",
   "/index.html",
   "/style.css",
   "/script.js",
-  "/templateicon1-192.png",
-  "/templateicon1-512.png",
+  "/UFCS-192.png",
+  "/UFCS-512.png",
   "/favicon.ico",
   "/manifest.json"
 ];
